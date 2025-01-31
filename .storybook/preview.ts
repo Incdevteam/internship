76ed4757fd6f3a5b5cbf40c8d419ee5cc21ae6c1
a-andrievskikh <sn0abg0@gmail.com>
2025-01-31 14:37:40 +0500

    chore: add inter font to preview.ts

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,10 @@
 import type { Preview } from '@storybook/react'
 
+import '@fontsource/inter/700.css'
+import '@fontsource/inter/600.css'
+import '@fontsource/inter/500.css'
+import '@fontsource/inter/400.css'
+
 const preview: Preview = {
   parameters: {
     controls: {
