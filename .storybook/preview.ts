c6aae01baf2a9a995ecfbdfaf9d7d40f1f5c6903
Petr Kanapelka <kanapelka.petr@gmail.com>
2025-02-05 12:59:06 +0300

    feat: update Storybook preview to use SCSS styles instead of global CSS
    Refs:JIR-71

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,6 +1,6 @@
 import type { Preview } from '@storybook/react'
 
-import '../src/app/globals.css'
+import '../src/app/styles/index.scss'
 import '@fontsource/inter/700.css'
 import '@fontsource/inter/600.css'
 import '@fontsource/inter/500.css'
