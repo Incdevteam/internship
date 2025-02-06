a82124622548616d0017a00772b88461c8e0dca8
MargaritaZh <margaritazhitkevich@gmail.com>
2025-02-06 22:00:39 +0300

    fix: refactor props in RadioGroup component Refs: JIR-92

--- src/shared/ui/radioGroup/RadioGroup.tsx
+++ src/shared/ui/radioGroup/RadioGroup.tsx
@@ -1,55 +1,3 @@
-// import { ComponentPropsWithoutRef, FC } from 'react'
-//
-// import { clsx } from 'clsx'
-// import { RadioGroup } from 'radix-ui'
-//
-// import s from './radioGroup.module.scss'
-//
-// import { Typography } from '../typography'
-//
-// /**
-//  * Represents a single option in the radio group.
-//  */
-// export interface Option {
-//   /** The text displayed next to the radio button. */
-//   label: string
-//   /** The value associated with this option. */
-//   value: string
-// }
-//
-// /**
-//  * Props for the `CustomRadioGroup` component.
-//  */
-// export type RadioGroupProps = {
-//   /** Array of options to display in the radio group. */
-//   options: Option[]
-//   /** Currently selected value in the radio group. */
-//   value?: string
-//   /**The name used when using this component inside a form*/
-//   name?: string
-//   /** Callback triggered when the selected value changes. */
-//   onValueChange?: (value: string) => void
-//   /** Whether the entire radio group is disabled. */
-//   disabled?: boolean
-// } & ComponentPropsWithoutRef<typeof RadioGroup.Root>
-//
-// export const CustomRadioGroup: FC<RadioGroupProps> = ({ options, ...rest }) => {
-//   const labelClasses = clsx(s.item, rest.disabled && s.disabled)
-//
-//   return (
-//     <RadioGroup.Root aria-label={'Aria label'} {...rest} className={s.root}>
-//       {options.map(el => (
-//         <Typography as={'label'} variant={'regular-small'} key={el.value} className={labelClasses}>
-//           <RadioGroup.Item value={el.value} className={s.radio}>
-//             <div className={s.frame}></div>
-//             <RadioGroup.Indicator className={s.indicator} />
-//           </RadioGroup.Item>
-//           {el.label}
-//         </Typography>
-//       ))}
-//     </RadioGroup.Root>
-//   )
-// }
 import { ComponentPropsWithoutRef } from 'react'
 
 import { clsx } from 'clsx'
@@ -82,7 +30,7 @@ export const CustomRadioGroup = (props: RadioGroupProps) => {
       {options.map(option => (
         <Typography
           as={'label'}
-          variant={'regular-small'}
+          variant={'regular-text-14'}
           key={option.value}
           className={labelClasses}
         >
