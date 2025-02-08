347c16453636b55bd360277b665144e3404f94ab
EmelyanovValery <109528891+EmelyanovValery@users.noreply.github.com>
2025-02-08 21:57:12 +0500

    fix: delete returned type of Card, refactor import ReactNode, сhange the order of styles Refs: JIR-73

--- src/shared/ui/card/Card.tsx
+++ src/shared/ui/card/Card.tsx
@@ -1,15 +1,15 @@
-import React, { ComponentPropsWithoutRef, JSX } from 'react'
+import { ComponentPropsWithoutRef, ReactNode } from 'react'
 
 import clsx from 'clsx'
 
 import styles from './card.module.scss'
 
 type Props = {
-  children: React.ReactNode
+  children: ReactNode
   className?: string
 } & ComponentPropsWithoutRef<'div'>
 
-export const Card = ({ children, className, ...props }: Props): JSX.Element => {
+export const Card = ({ children, className, ...props }: Props) => {
   return (
     <div className={clsx(className, styles.root)} {...props}>
       {children}
